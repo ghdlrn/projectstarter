1a5e44b0709181d8e411a2f2baecaebe8443543f
ghdlrn <ghdlrn@gmail.com>
2024-04-18 17:39:16 +0900

    commit

--- src/main/java/lkm/starterproject/dto/UserDto.java
+++ src/main/java/lkm/starterproject/dto/UserDto.java
@@ -1,8 +1,4 @@
 package lkm.starterproject.dto;
 
-public record UserDto(
-        String name,
-        String email,
-        String password
-) {
+public record UserDto(String name, String email, String password) {
 }
